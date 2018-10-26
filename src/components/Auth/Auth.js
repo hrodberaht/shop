76036db4a1d4baa7d618ca662444a957ba9f2853
hrodberaht <robpszczolka@gmail.com>
2018-10-26 17:28:25 +0200

    added validation by yup

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ConnectedLoginForm from './LoginForm';
-import Registration from './Registration';
+import ConnectedRegistration from './Registration';
 
 export default class Auth extends Component {
   render() {
@@ -10,7 +10,7 @@ export default class Auth extends Component {
         <Router>
           <Switch>
             <Route exact path="/" component={ConnectedLoginForm} />
-            <Route path="/registration" component={Registration} />
+            <Route path="/registration" component={ConnectedRegistration} />
             <Route component={() => <p>Not Found</p>} />
           </Switch>
         </Router>
--- src/components/Auth/Registration.js
+++ src/components/Auth/Registration.js
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email()
+    .required(),
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  password: yup
+    .string()
+    .required()
+    .min(6)
+    .max(16),
+  companie: yup.string().required(),
+});
+const asyncValidate = values => schema.validate(values, { abortEarly: false }).catch((err) => {
+  const errorsForm = {};
+  err.inner.forEach((error) => {
+    errorsForm[error.path] = error.type;
+  });
+  throw errorsForm;
+});
 
 export class Registration extends Component {
   submit = (values) => {
@@ -26,17 +49,17 @@ export class Registration extends Component {
           </label>
           <label className="form-label" htmlFor="first-name">
             <p>First name:</p>
-            <Field name="first-name" type="text" component={this.renderField} label="First name" />
+            <Field name="firstName" type="text" component={this.renderField} label="First name" />
           </label>
           <label className="form-label" htmlFor="last-name">
             <p>Last name:</p>
-            <Field name="last-name" type="text" component={this.renderField} label="Last name" />
+            <Field name="lastName" type="text" component={this.renderField} label="Last name" />
           </label>
           <label className="form-label" htmlFor="password">
             <p>Password:</p>
             <Field name="password" type="password" component={this.renderField} label="Password" />
           </label>
-          <Field name="companies" component="select">
+          <Field name="companie" component="select">
             <option />
             <option value="com1">com1</option>
             <option value="com2">com2</option>
@@ -51,5 +74,6 @@ export class Registration extends Component {
   }
 }
 export default reduxForm({
+  asyncValidate,
   form: 'registration',
 })(Registration);
