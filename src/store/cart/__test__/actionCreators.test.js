f1ff3e51a9e7b9ac61d478c05f7044f3eb94d7ec
hrodberaht <robpszczolka@gmail.com>
2018-11-16 16:51:42 +0100

    added reducer tests

--- src/store/cart/__test__/actionCreators.test.js
+++ src/store/cart/__test__/actionCreators.test.js
@@ -5,7 +5,6 @@ describe('cart actions', () => {
   it('should call clearCart when type CLEAR_CART', () => {
     const expectedActions = {
       type: types.CLEAR_CART,
-      empty: [],
     };
 
     expect(clearCart()).toEqual(expectedActions);
