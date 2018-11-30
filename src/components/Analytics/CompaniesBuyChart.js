a812fbe7c64761566ae870e6e15456388e1634b1
hrodberaht <robpszczolka@gmail.com>
2018-11-30 14:22:59 +0100

    add charts

--- src/components/Analytics/CompaniesBuyChart.js
+++ src/components/Analytics/CompaniesBuyChart.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
+import Highcharts from 'highcharts';
+import Exporting from 'highcharts/modules/exporting';
+
+Exporting(Highcharts);
 
 export default class CompaniesBuyChart extends Component {
+  componentDidMount() {
+    Highcharts.chart('companies-buy-chart', {
+      chart: {
+        type: 'bar',
+      },
+      title: {
+        text: 'Sells',
+      },
+      xAxis: {
+        categories: ['Sumary'],
+      },
+      yAxis: {
+        title: {
+          text: 'Sells',
+        },
+      },
+      series: [
+        {
+          name: 'IBM',
+          data: [2400],
+        },
+        {
+          name: 'Apple',
+          data: [4800],
+        },
+      ],
+    });
+  }
+
   render() {
-    return <div>Companies</div>;
+    return <div id="companies-buy-chart" />;
   }
 }
--- src/components/Analytics/SellChart.js
+++ src/components/Analytics/SellChart.js
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
+import Highcharts from 'highcharts';
+import Exporting from 'highcharts/modules/exporting';
+
+Exporting(Highcharts);
 
 export default class SellChart extends Component {
+  componentDidMount() {
+    Highcharts.chart('sell-chart', {
+      chart: {
+        type: 'pie',
+      },
+      title: {
+        text: 'Fruit Consumption',
+      },
+      series: [
+        {
+          name: 'Brands',
+          colorByPoint: true,
+          data: [
+            {
+              name: 'Prod1',
+              y: 62.0,
+            },
+            {
+              name: 'Prod2',
+              y: 18.0,
+            },
+            {
+              name: 'Prod3',
+              y: 15.0,
+            },
+            {
+              name: 'Prod4',
+              y: 5.0,
+            },
+          ],
+        },
+      ],
+    });
+  }
+
   render() {
-    return <div>SellCharts</div>;
+    return <div id="sell-chart" />;
   }
 }
