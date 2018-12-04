484cecbe5d8075b0479d4bb379731feb6f8d5c40
hrodberaht <robpszczolka@gmail.com>
2018-12-04 09:28:14 +0100

    show correct data in sells

--- src/shared/companiesBuyChart.js
+++ src/shared/companiesBuyChart.js
@@ -4,7 +4,16 @@ import Exporting from 'highcharts/modules/exporting';
 Exporting(Highcharts);
 
 const companiesBuyChart = (data) => {
-  const companies = data.orders.filter(order => order.companyId !== '');
+  const companies = [...data.orders].filter(order => order.companyId !== '');
+  const series = [];
+  companies.forEach((company) => {
+    if (series.length === 0) return series.push(company);
+    const index = series.findIndex(serie => serie.companyId === company.companyId);
+    // eslint-disable-next-line no-return-assign
+    return index > 0
+      ? (series[index] = { ...company, totalPrice: series[index].totalPrice + company.totalPrice })
+      : series.push(company);
+  });
   Highcharts.chart('companies-buy-chart', {
     chart: {
       type: 'bar',
@@ -20,7 +29,7 @@ const companiesBuyChart = (data) => {
         text: 'Sells',
       },
     },
-    series: companies.map(company => ({ name: company.companyId, data: [company.totalPrice] })),
+    series: series.map(company => ({ name: company.companyId, data: [company.totalPrice] })),
   });
 };
 
