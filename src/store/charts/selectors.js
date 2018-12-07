0b832f9fd70c5383293fa1becc6ee98265133845
hrodberaht <robpszczolka@gmail.com>
2018-12-07 11:00:37 +0100

    add deep equal in selector

--- src/store/charts/selectors.js
+++ src/store/charts/selectors.js
@@ -1,11 +1,13 @@
-import { createSelector } from 'reselect';
+import { createSelectorCreator, defaultMemoize } from 'reselect';
+import { isEqual } from 'lodash';
 
 const getSoldList = ({ charts }) => charts.soldProductsList.list;
 const getSoldProducts = ({ charts }) => charts.soldProductsList.byId;
 
-export const getChartsDataSoldProducts = createSelector(
-  getSoldList,
-  getSoldProducts,
+const createDeepEqualSelector = createSelectorCreator(defaultMemoize, isEqual);
+
+export const getChartsDataSoldProducts = createDeepEqualSelector(
+  [getSoldList, getSoldProducts],
   (list, products) => list.map(id => ({
     name: products[id].name,
     y: products[id].pcsOrder,
