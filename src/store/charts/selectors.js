4609cb72cd4c9d355affac1726bda984e3e29bb9
hrodberaht <robpszczolka@gmail.com>
2018-12-07 09:39:59 +0100

    add reselect

--- src/store/charts/selectors.js
+++ src/store/charts/selectors.js
@@ -1,9 +1,15 @@
-export const getChartsDataSoldProducts = ({ charts }) => {
-  const { list, byId } = charts.soldProductsList;
-  return list.map(id => ({
-    name: byId[id].name,
-    y: byId[id].pcsOrder,
-  }));
-};
+import { createSelector } from 'reselect';
+
+const getSoldList = ({ charts }) => charts.soldProductsList.list;
+const getSoldProducts = ({ charts }) => charts.soldProductsList.byId;
+
+export const getChartsDataSoldProducts = createSelector(
+  getSoldList,
+  getSoldProducts,
+  (list, products) => list.map(id => ({
+    name: products[id].name,
+    y: products[id].pcsOrder,
+  })),
+);
 
 export default getChartsDataSoldProducts;
