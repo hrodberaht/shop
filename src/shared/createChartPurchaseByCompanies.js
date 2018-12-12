791d56cd7a3118472e020e936cf7ea8736f2a9fd
hrodberaht <robpszczolka@gmail.com>
2018-12-12 10:54:18 +0100

    remove eslint disable

--- src/shared/createChartPurchaseByCompanies.js
+++ src/shared/createChartPurchaseByCompanies.js
@@ -3,14 +3,18 @@ import Exporting from 'highcharts/modules/exporting';
 
 Exporting(Highcharts);
 
-const createChartPurchaseByCompanies = (data) => {
+const createChartPurchaseByCompanies = data => {
   const series = [];
-  data.orders.forEach((company) => {
+  data.orders.forEach(company => {
     if (series.length === 0) return series.push(company);
-    const index = series.findIndex(serie => serie.companyId === company.companyId);
-    // eslint-disable-next-line no-return-assign
+    const index = series.findIndex(
+      serie => serie.companyId === company.companyId,
+    );
     return index > 0
-      ? (series[index] = { ...company, totalPrice: series[index].totalPrice + company.totalPrice })
+      ? (series[index] = {
+          ...company,
+          totalPrice: series[index].totalPrice + company.totalPrice,
+        })
       : series.push(company);
   });
   Highcharts.chart('companies-buy-chart', {
@@ -28,7 +32,10 @@ const createChartPurchaseByCompanies = (data) => {
         text: 'Sells',
       },
     },
-    series: series.map(company => ({ name: company.companyId, data: [company.totalPrice] })),
+    series: series.map(company => ({
+      name: company.companyId,
+      data: [company.totalPrice],
+    })),
   });
 };
 
--- src/shared/createDataForSoldChart.js
+++ src/shared/createDataForSoldChart.js
@@ -1,18 +1,25 @@
-const createDataForSoldChart = (data) => {
+const createDataForSoldChart = data => {
   const productsData = [];
-  data.forEach(order => order.productsOrder.forEach((product) => {
-    if (productsData.length === 0) {
-      productsData.push({ ...product, pcsOrder: +product.pcsOrder });
-    }
-    const findedProduct = productsData.find(
-      productItem => productItem.productId === product.productId,
-    );
-      // eslint-disable-next-line no-return-assign
-    return findedProduct
-      ? ((findedProduct.pcsOrder += +product.pcsOrder),
-      (findedProduct.totalPrice += +product.totalPrice))
-      : productsData.push({ ...product, pcsOrder: +product.pcsOrder });
-  }));
+  data.forEach(order =>
+    order.productsOrder.forEach(product => {
+      if (productsData.length === 0) {
+        productsData.push({
+          ...product,
+          pcsOrder: +product.pcsOrder,
+        });
+      }
+      const findedProduct = productsData.find(
+        productItem => productItem.productId === product.productId,
+      );
+      return findedProduct
+        ? ((findedProduct.pcsOrder += +product.pcsOrder),
+          (findedProduct.totalPrice += +product.totalPrice))
+        : productsData.push({
+            ...product,
+            pcsOrder: +product.pcsOrder,
+          });
+    }),
+  );
 
   return productsData;
 };
