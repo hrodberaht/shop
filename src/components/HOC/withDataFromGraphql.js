53facedb85da1348848c8a92903ff94f008677c1
hrodberaht <robpszczolka@gmail.com>
2018-12-13 08:22:05 +0100

    refactor

--- src/components/HOC/withDataFromGraphql.js
+++ src/components/HOC/withDataFromGraphql.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 const withDataFromGraphql = (WrappedComponent) => {
   class WithDataFromGraphql extends Component {
     render() {
-      if (this.props.fetchedData.loading) return <p>Loading...</p>;
-      if (this.props.fetchedData.error) return <p>Error :(</p>;
+      const { loading, error } = this.props.fetchedData;
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
       return <WrappedComponent {...this.props} />;
     }
   }
