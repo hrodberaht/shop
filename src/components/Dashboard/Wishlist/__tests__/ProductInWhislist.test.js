24bf3694014a2c463b4e986e26e5478bb1b64fe3
hrodberaht <robpszczolka@gmail.com>
2018-12-17 15:39:41 +0100

    add tests

--- src/components/Dashboard/Wishlist/__tests__/ProductInWhislist.test.js
+++ src/components/Dashboard/Wishlist/__tests__/ProductInWhislist.test.js
@@ -9,6 +9,7 @@ const defaultProps = {
     inStock: '30',
   },
   remove: jest.fn(),
+  handleClickToCart: jest.fn(),
 };
 
 const setup = buildSetup(ProductInWishlist, defaultProps);
@@ -19,8 +20,28 @@ describe('ProductInWishlist />', () => {
     expect(wrapper).toMatchSnapshot();
   });
   it('should call remove after click', () => {
-    const { wrapper, props } = setup();
-    wrapper.find('button').simulate('click');
-    expect(wrapper.instance().props.remove).toHaveBeenCalledWith(props.product.productId);
+    const {
+      wrapper,
+      props: {
+        product: { productId },
+      },
+    } = setup();
+    wrapper
+      .find('button')
+      .at(0)
+      .simulate('click');
+    expect(wrapper.instance().props.remove).toHaveBeenCalledWith(productId);
+  });
+
+  it('should call handleClickToCart when click', () => {
+    const {
+      wrapper,
+      props: { product },
+    } = setup();
+    wrapper
+      .find('button')
+      .at(1)
+      .simulate('click');
+    expect(wrapper.instance().props.handleClickToCart).toHaveBeenCalledWith(product);
   });
 });
