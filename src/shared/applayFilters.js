70fd7e21d94d4db74f9a9a5cc4df0f30d5820b16
hrodberaht <robpszczolka@gmail.com>
2018-12-21 09:28:59 +0100

    fix bug

--- src/shared/applayFilters.js
+++ src/shared/applayFilters.js
@@ -2,7 +2,7 @@ const applayFiletrs = (data, filterValues) => {
   if (Object.keys(filterValues).length === 0) {
     return data;
   }
-  const regexp = new RegExp(filterValues.id, 'i');
+  const regexp = new RegExp(filterValues.id.replace(/[/\\^$*+?.()|[\]{}]/g, '\\$&'), 'i');
   const filteredByStatus = filterValues.status !== 'all'
     ? data.filter(order => order.status === filterValues.status)
     : data;
