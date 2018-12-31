1eae1756ac22655705837812081873e3bc7e5116
hrodberaht <robpszczolka@gmail.com>
2018-12-31 10:53:52 +0100

    add button toggle add invoice

--- src/components/Dashboard/Documents/Invoices.js
+++ src/components/Dashboard/Documents/Invoices.js
@@ -3,12 +3,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 export class Invoices extends Component {
+  state = {
+    isVisableAddInvoice: false,
+  };
+
   static propTypes = {
     prop: PropTypes,
   };
 
+  toggleAddInvoice = () => {
+    this.setState(state => ({ isVisableAddInvoice: !state.isVisableAddInvoicee }));
+  };
+
   render() {
-    return <div>Invoices</div>;
+    return (
+      <div>
+        <button type="button" onClick={this.toggleAddInvoice}>
+          Add invoice
+        </button>
+        {this.state.isVisableAddInvoice && <p>Form</p>}
+      </div>
+    );
   }
 }
 
